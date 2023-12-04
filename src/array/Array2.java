840e5ef8ad1c1818430faafc23371ef1a5e06033
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-12-04 19:28:26 +0600

    Java class 9th commit, array max & min number find

--- src/array/Array2.java
+++ src/array/Array2.java
@@ -5,20 +5,55 @@ import java.util.Scanner;
 
 public class Array2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double[] num = new double[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] array = new double[5];
+        System.out.println("array length : " + array.length);
+
         double sum = 0;
-        System.out.println("enter 5 numbere ");
+        System.out.print("enter 5 numbers : ");
         for (int i= 0; i<5; i++){
-            num[i] = input.nextDouble();
+            array[i] = sc.nextDouble();
         }
 
         for(int i = 0; i<5; i++){
-            sum = sum + num[i];
+            sum = sum + array[i];
         }
-        System.out.println("the sum number is "+sum);
+        System.out.println("the sum number is : "+sum);
+
+        double average = sum/array.length;
+        System.out.println("average : "+average);
+
+        ////// array max & min number find
+        double max = array[0];
+        double min = array[0];
+        for (int i=0; i<array.length; i++){
+            if (max<array[i]){
+                max=array[i];
+            }
+            if (min>array[i]){
+                min=array[i];
+            }
+
+        }
+        System.out.println("max : "+max);
+        System.out.println("min : "+min);
+
+
+        ////// array min number find
+        double min2 = array[0];
+        for (int i=0; i<array.length; i++){
+            if (min2>array[i]){
+                min2=array[i];
+            }
+        }
+        System.out.println("min2 : "+min2);
+
+
+
+
+
 
     }
-
-    
 }
